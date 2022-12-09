8ec6fb4580beb8d0d105ca35ff972e3e6eb78b5f
Carlos Asur Bustillo Zamora <asurbz@gmail.com>
2022-12-09 01:38:09 -0600

    Initial commit

---
+++ Controller/PrintNumber.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMaycoDupplo.Controller
+{
+    public static class PrintNumber
+    {
+        /// <summary>
+        /// Return specific text
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> Texts() => new() { "first", "last" };
+
+        /// <summary>
+        /// Returns the first and last number (of the document)
+        /// </summary>
+        /// <returns></returns>
+        public static List<int> StaticNumbers() => new() { 1, 150 };
+
+        /// <summary>
+        /// Validate if the first number is equal to or less than the last to continue
+        /// </summary>
+        /// <param name="firstNumber">The first number given by the user.</param>
+        /// <param name="lastNumber">The last number given by the user.</param>
+        /// <param name="msg">If there is an error a message is returned</param>
+        /// <returns></returns>
+        public static bool ValidateFirstvsLast(int firstNumber, int lastNumber, out string msg)
+        {
+            try
+            {
+                bool result = firstNumber <= lastNumber;
+
+                if (result)
+                    msg = "";
+                else
+                    msg = "Error: The first number is greater than the last";
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Validate if number is empty, null or not an integer
+        /// </summary>
+        /// <param name="value">The number given by the user</param>
+        /// <param name="msg">If there is an error a message is returned</param>
+        /// <returns></returns>
+        public static bool ValidateNumber(string? value, out string msg)
+        {
+			try
+			{
+                bool result = !string.IsNullOrEmpty(value) && int.TryParse(value, out _);
+
+                if (result)
+                    msg = "";
+                else
+                    msg = "Error: The {0} number is empty, null or not an integer";
+
+                return result;
+            }
+			catch (Exception)
+			{
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Function to return the number or text
+        /// </summary>
+        /// <param name="value">The number to validate</param>
+        /// <returns></returns>
+        public static string PrintingNumber(int value)
+        {
+            try
+            {
+                //REGLAS:
+
+                //Cualquier número      regresa el número:
+                //Multiplos del 3       regresa Mayco
+                //Multiplos del 5       regresa Dupplo
+                //Multiplos del 3 y 5   regresa MaycoDupplo
+
+                if (value % 3 == 0 && value % 5 == 0)
+                    return "MaycoDupplo";
+                else if (value % 3 == 0)
+                    return "Mayco";
+                else if (value % 5 == 0)
+                    return "Dupplo";
+                else
+                    return value.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppMaycoDupplo.Controller;
+
+try
+{
+    List<int> listNumbers = new();
+
+
+    /***NOTE:***/
+    /***If you need to do this dynamically, comment out the static and uncomment the dynamic or vice versa***/
+
+
+    #region Dynamic
+
+    //string msg;
+
+    //var texts = PrintNumber.Texts();
+    //for (int i = 0; i < texts.Count; i++)
+    //{
+    //    Console.WriteLine(string.Format("Write the {0} number:", texts[i].ToString()));
+    //    string? readNumber = Console.ReadLine();
+
+    //    if (!PrintNumber.ValidateNumber(readNumber, out msg))
+    //    {
+    //        Console.WriteLine(string.Format(msg, texts[i].ToString()));
+    //        return;
+    //    }
+
+    //    listNumbers.Add(Convert.ToInt32(readNumber));
+    //}
+
+    //if (!PrintNumber.ValidateFirstvsLast(listNumbers[0], listNumbers[1], out msg))
+    //{
+    //    Console.WriteLine(msg);
+    //    return;
+    //}
+
+    #endregion Dynamic
+
+    #region Static
+
+    listNumbers = PrintNumber.StaticNumbers();
+
+    #endregion Static
+
+    if (listNumbers.Count == 0)
+        return;
+    else
+        Console.WriteLine("");
+
+    /*Print the numbers*/
+    for (int i = listNumbers[0]; i <= listNumbers[1]; i++)
+    {
+        Console.WriteLine(PrintNumber.PrintingNumber(i));
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
